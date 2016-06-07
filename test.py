e203054437101b4fc5d368cc00148fafda4d35ce
Jim Carr <jim.carr@moonbowlabs.com>
2016-06-07 19:24:04 -0400

    Method name refactoring.

--- test.py
+++ test.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
 	testLongitude = '-84.192'
 	
 	myAstro = AstroNowLib.CAstroNow(lat=testLatitude, long=testLongitude, prettyprint=True)
-	#myAstro = AstroLib.CAstro(lat=testLatitude, long=testLongitude, prettyprint=True, calcdate="2016/01/17 20:00:00")
+	#myAstro = AstroNowLib.CAstroNow(lat=testLatitude, long=testLongitude, prettyprint=True, calcdate="2016/01/17 20:00:00")
 
 	print myAstro.GetCurrentConditions()
-	
+	#print myAstro.GetPlanetsInfo()
+	#print myAstro.GetPlanetInfo("Mars")
+	#print myAstro.GetMoonInfo()	
+	#print myAstro.GetStarInfo("Sirius")
+	#print myAstro.GetStarsInfo()
+	#print myAstro.GetSunInfo()
