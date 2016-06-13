22d194453867815336885349522846459a51b0d6
U-CARESOURCE\jfcarr <jfcarr@4000337L.CareSource.corp>
2016-06-13 12:27:55 -0400

    Tests for astro_util module.

---
+++ test_au.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import astro_util as AU
+
+if __name__ == '__main__':
+	print AU.AzimuthToCompassDirection(AU.DegreesToRadians(270))
+	#print AU.DegreesToRadians(1)
+	#print AU.RadiansToDegrees(.0174532925199)
