7f73951ff8d4b50855a653b06962b481a2e583c2
jfcarr <jfcarr@gmail.com>
2017-06-14 21:19:28 -0400

    Added command line argument support.

--- client_cl.py
+++ client_cl.py
@@ -1,25 +1,68 @@
 #!/usr/bin/python3
 
+import argparse
 import astro_now_lib as AL
 import geocode as GC
 import sys
 
 def main(args):
-	myGeocoder = GC.CGeocode()
-	myCoordinates = myGeocoder.GetCoordinatesForCity('Dayton, OH')
-	
-	myAstro = AL.CAstroNow(lat=str(myCoordinates['Latitude']), long=str(myCoordinates['Longitude']), prettyprint=True)
-	#myAstro = AL.CAstroNow(lat=str(myCoordinates['Latitude']), long=str(myCoordinates['Longitude']), prettyprint=True, calcdate="2016/06/12 23:35:00")
+	parser = argparse.ArgumentParser()
 
-	#print(myAstro.GetCurrentConditions())
-	#print(myAstro.GetPlanetsInfo())
-	#print(myAstro.GetPlanetInfo("Mars"))
-	#print(myAstro.GetMoonInfo())
-	#print(myAstro.GetStarInfo("Sirius"))
-	#print(myAstro.GetStarsInfo())
-	#print(myAstro.GetSunInfo())
-	#print(myAstro.GetTwilight())
-	print(myAstro.GetObjectInfo("KIC 8462852","20:6:15","44:27:25",11))
+	try:
+
+		parser.add_argument("-c", "--current", help="Info for all known objects.", action="store_true")
+		parser.add_argument("-d", "--date", type=str, help="Date and time, in the form 'yyyy/mm/dd hh:mm:ss', e.g., '2017/06/11 22:15:00'.  If omitted, current date/time is used.")
+		parser.add_argument("-l", "--location", type=str, help="Location, in the form 'City, State', e.g., 'Dayton, OH'")
+		parser.add_argument("-m", "--moon", help="Moon info.", action="store_true")
+		parser.add_argument("-o", "--object", type=str, help="Info for a custom object, in the form 'name','right ascension','declination','magnitude', e.g., 'KIC8462852,20:6:15,44:27:25,11'")
+		parser.add_argument("-p", "--planet", type=str, help="Info for individual planet, or 'all'")
+		parser.add_argument("-s", "--star", type=str, help="Info for individual star, or 'all' for 50 brightest stars.")
+		parser.add_argument("-su", "--sun", help="Sun info.", action="store_true")
+		parser.add_argument("-t", "--twilight", help="Twilight state.", action="store_true")
+		args = parser.parse_args()
+	except Exception as ex:
+		print(ex)
+		sys.exit(1)
+
+	if args.location:
+		myGeocoder = GC.CGeocode()
+		myCoordinates = myGeocoder.GetCoordinatesForCity(args.location)
+	else:
+		print("Location is required.")
+		sys.exit(-1)
+
+	if args.date:
+		myAstro = AL.CAstroNow(lat=str(myCoordinates['Latitude']), long=str(myCoordinates['Longitude']), prettyprint=True, calcdate=args.date)
+	else:
+		myAstro = AL.CAstroNow(lat=str(myCoordinates['Latitude']), long=str(myCoordinates['Longitude']), prettyprint=True)
+
+	if args.current:
+		print(myAstro.GetCurrentConditions())
+
+	if args.planet:
+		if args.planet.strip() == "all":
+			print(myAstro.GetPlanetsInfo())
+		else:
+			print(myAstro.GetPlanetInfo(args.planet.strip()))
+
+	if args.moon:
+		print(myAstro.GetMoonInfo())
+
+	if args.star:
+		if args.star.strip() == "all":
+			print(myAstro.GetStarsInfo())
+		else:
+			print(myAstro.GetStarInfo(args.star.strip()))
+
+	if args.sun:
+		print(myAstro.GetSunInfo())
+
+	if args.twilight:
+		print('Twilight state: ' + myAstro.GetTwilight())
+
+	if args.object:
+		object_args = args.object.split(',')
+		print(myAstro.GetObjectInfo(object_args[0],object_args[1],object_args[2],object_args[3]))
 
 	return 0
 
